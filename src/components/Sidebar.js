ea3a90e3edb74b82ee83445fe53103f1dc726f0a
devsilva <jerson.silva99@hotmail.com>
2023-02-05 13:49:56 -0500

    feat: Create sidebar component with icons and text

--- src/components/Sidebar.js
+++ src/components/Sidebar.js
@@ -1,5 +1,13 @@
 import Image from 'next/image'
 
+import SidebarLink from 'components/SidebarLink'
+import Notification from 'components/Icons/Notification'
+
+import { BiHash } from 'react-icons/bi'
+import { RiHome7Fill } from 'react-icons/ri'
+import { BsBookmark } from 'react-icons/bs'
+import { HiOutlineInbox, HiOutlineClipboardList, HiOutlineUser, HiOutlineDotsCircleHorizontal } from 'react-icons/hi'
+
 const Sidebar = () => {
   return (
     <>
@@ -11,6 +19,16 @@ const Sidebar = () => {
             height={30}
             alt='Logo twitter'
           />
+        </div>
+        <div className='space-y-2.5 mt-4 mb-2.5 xl:ml-24'>
+          <SidebarLink text='Home' Icon={RiHome7Fill} active />
+          <SidebarLink text='Explore' Icon={BiHash} />
+          <SidebarLink text='Notifications' Icon={Notification} />
+          <SidebarLink text='Messages' Icon={HiOutlineInbox} />
+          <SidebarLink text='Bookmarks' Icon={BsBookmark} />
+          <SidebarLink text='Lists' Icon={HiOutlineClipboardList} />
+          <SidebarLink text='Profile' Icon={HiOutlineUser} />
+          <SidebarLink text='More' Icon={HiOutlineDotsCircleHorizontal} />
         </div>
       </div>
     </>
