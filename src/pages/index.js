46b50cda3eed21972c74c7f2878c7bacf5ef57b5
devsilva <jerson.silva99@hotmail.com>
2023-02-04 23:54:28 -0500

    refactor: Resolve problem of linter

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,16 @@
 import Head from 'next/head'
 
 const Home = () => {
-  return <>
-    <Head>
-      <title>Twitter</title>
-      <meta name='description' content='Twitter clone with Nextjs and firebase 9' />
-      <meta name='viewport' content='width=device-width, initial-scale=1' />
-      <link rel='icon' href='/favicon.png' />
-    </Head>
-  </>
+  return (
+    <>
+      <Head>
+        <title>Twitter</title>
+        <meta name='description' content='Twitter clone with Nextjs and firebase 9' />
+        <meta name='viewport' content='width=device-width, initial-scale=1' />
+        <link rel='icon' href='/favicon.png' />
+      </Head>
+    </>
+  )
 }
 
 export default Home
