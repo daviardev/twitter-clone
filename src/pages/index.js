f888c31070828a84c14953d13f8369149940c98d
devsilva <jerson.silva99@hotmail.com>
2023-02-05 17:53:44 -0500

    refactor: Add title home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ const Home = () => {
   return (
     <>
       <Head>
-        <title>Twitter</title>
+        <title>Home / Twitter</title>
         <meta name='description' content='Twitter clone with Nextjs and firebase 9' />
         <meta name='viewport' content='width=device-width, initial-scale=1' />
         <link rel='icon' href='/favicon.png' />
