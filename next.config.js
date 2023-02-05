2b504810b78478cd6da2147f51b882c832665e2f
devsilva <jerson.silva99@hotmail.com>
2023-02-04 23:08:35 -0500

    feat: Upload initial files

---
+++ next.config.js
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+
+const nextConfig = {
+  // Config next
+}
+
+module.exports = nextConfig
---
+++ src/pages/index.js
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+const Home = () => {
+  return <>
+    <Head>
+      <title>Twitter</title>
+      <meta name='description' content='Twitter clone with Nextjs and firebase 9' />
+      <meta name='viewport' content='width=device-width, initial-scale=1' />
+      <link rel='icon' href='/favicon.png' />
+    </Head>
+  </>
+}
+
+export default Home
