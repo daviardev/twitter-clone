010b3b209c359ab980bad573c1f1996f688ff664
devsilva <jerson.silva99@hotmail.com>
2023-02-15 13:37:22 -0500

    feat: Make connection with next auth and google

---
+++ src/pages/api/auth/[...nextauth].js
@@ -0,0 +1,26 @@
+import NextAuth from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  providers: [
+    GoogleProvider({
+      clientId: process.env.NEXT_PUBLIC_CLIENTKEY,
+      clientSecret: process.env.NEXT_PUBLIC_SECRETKEY
+    })
+    // ...add more providers here
+  ],
+  callbacks: {
+    async session ({ session, token }) {
+      session.user.tag = session.user.name
+        .split(' ')
+        .join('')
+        .toLocaleLowerCase()
+
+      session.user.uid = token.sub
+      return session
+    }
+  }
+}
+
+export default NextAuth(authOptions)
