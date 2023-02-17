985423bfd9c00550694161cf07ddfa3b39599e72
devsilva <jerson.silva99@hotmail.com>
2023-02-17 01:15:46 -0500

    feat: Add funcionality like, time and more icons

--- src/components/Posts.js
+++ src/components/Posts.js
@@ -1,23 +1,63 @@
 import { useState, useEffect } from 'react'
 import { useRouter } from 'next/router'
 
-import Moment from 'moment'
-
 import { db } from 'firebase/client'
 import { collection, deleteDoc, doc, onSnapshot, orderBy, query, setDoc } from 'firebase/firestore'
 
-import { useSession } from 'next-auth/react'
+import { modalState } from 'atoms/modalAtom'
 
-import { BsTrash } from 'react-icons/bs'
+import { useSession } from 'next-auth/react'
+import { useRecoilState } from 'recoil'
+import Moment from 'react-moment'
+
+import { BsTrash, BsHeart, BsHeartFill } from 'react-icons/bs'
 import { BiShareAlt } from 'react-icons/bi'
-import { HiDotsHorizontal, HiOutlineChartBar, HiSwitchHorizontal } from 'react-icons/hi'
+import { HiDotsHorizontal, HiOutlineChartBar, HiSwitchHorizontal, HiOutlineChat } from 'react-icons/hi'
 
 const Posts = ({ id, post, postPage }) => {
+  const [liked, setLiked] = useState(false)
+  const [likes, setLikes] = useState([])
+  const [comments, setComments] = useState([])
+
+  const [postId, setPostId] = useRecoilState(modalState)
+  const [isOpen, setIsOpen] = useRecoilState(modalState)
+
   const { data: session } = useSession()
   const router = useRouter()
+
+  // Send like to posts
+  useEffect(
+    () =>
+      onSnapshot(collection(db, 'posts', id, 'likes'), (snapshot) =>
+        setLikes(snapshot.docs)
+      ),
+    [db, id]
+  )
+
+  // Aument 1 like when the user click in the buton like
+
+  useEffect(
+    () =>
+      setLiked(
+        likes.findIndex((like) => like.id === session?.user?.uid) !== -1
+      ),
+    [likes]
+  )
+
+  // When the user set like, like post, if dont like, remove like
+
+  const likePost = async () => {
+    liked
+      ? await deleteDoc(doc(db, 'posts', id, 'likes', session.user.uid))
+      : await setDoc(doc(db, 'posts', id, 'likes', session.user.uid), { username: session.user.name })
+  }
+
   return (
     <>
-      <div className='p-3 fkex cursor-pointer border-b border-gray-700 text-white'>
+      <div
+        className='p-3 flex cursor-pointer border-b border-gray-700'
+        onClick={() => router.push(`/${id}`)}
+      >
         {!postPage && (
           <img
             src={post?.userImg}
@@ -41,7 +81,7 @@ const Posts = ({ id, post, postPage }) => {
               </div>{' '}
               .{' '}
               <span className='hover:underline text-sm sm:text-[15px]'>
-                {/* <Moment fromNow>{post?.timestamp?.toDate()}</Moment> */}
+                <Moment fromNow={post.createdAt?.toDate()} />
               </span>
               {!postPage && (
                 <p className='text-[#d9d9d9] text-[15px] sm:text-base mt-0.5'>{post?.text}</p>
@@ -60,7 +100,7 @@ const Posts = ({ id, post, postPage }) => {
             className='rounded-2xl max-h-[700px] object-cover mr-2'
           />
           <div className={`text-[#6e767d] flex justify-between w-10/12 ${postPage && 'mx-auto'}`}>
-            {/* <div
+            <div
               className='flex items-center space-x-1 group'
               onClick={(e) => {
                 e.stopPropagation()
@@ -69,14 +109,14 @@ const Posts = ({ id, post, postPage }) => {
               }}
             >
               <div className='icon group-hover:bg-[#1d9bf0] group-hover:bg-opacity-10'>
-                <HiChat className='h-5 group-hover:text-[#1d9bf0]' />
+                <HiOutlineChat className='h-5 group-hover:text-[#1d9bf0]' />
               </div>
               {comments.length > 0 && (
                 <span className='group-hover:text-[#1d9bf0] text-sm'>
                   {comments.length}
                 </span>
               )}
-            </div> */}
+            </div>
 
             {session.user.uid === post?.id
               ? (
@@ -101,7 +141,7 @@ const Posts = ({ id, post, postPage }) => {
                 </div>
                 )}
 
-            {/* <div
+            <div
               className='flex items-center space-x-1 group'
               onClick={(e) => {
                 e.stopPropagation()
@@ -111,10 +151,10 @@ const Posts = ({ id, post, postPage }) => {
               <div className='icon group-hover:bg-pink-600/10'>
                 {liked
                   ? (
-                    <HeartIconFilled className='h-5 text-pink-600' />
+                    <BsHeartFill className='h-5 text-pink-600' />
                     )
                   : (
-                    <HeartIcon className='h-5 group-hover:text-pink-600' />
+                    <BsHeart className='h-5 group-hover:text-pink-600' />
                     )}
               </div>
               {likes.length > 0 && (
@@ -126,7 +166,7 @@ const Posts = ({ id, post, postPage }) => {
                   {likes.length}
                 </span>
               )}
-            </div> */}
+            </div>
 
             <div className='icon group'>
               <BiShareAlt className='h-5 group-hover:text-[#1d9bf0]' />
