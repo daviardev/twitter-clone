24dec826fb4746d893ba528bb38398728c9fc313
devsilva <jerson.silva99@hotmail.com>
2023-02-16 20:42:32 -0500

    feat: Render posts and add blur to header

--- src/components/Feed.js
+++ src/components/Feed.js
@@ -7,6 +7,7 @@ import Input from 'components/Input'
 import { db } from 'firebase/client'
 
 import { collection, onSnapshot, orderBy, query } from 'firebase/firestore'
+import Posts from './Posts'
 
 const Feed = () => {
   const [posts, setPosts] = useState([])
@@ -23,7 +24,7 @@ const Feed = () => {
   return (
     <>
       <div className='flex-grow border-l border-r border-gray-700 max-w-2xl sm:ml-[73px] xl:ml-[370px]'>
-        <div className='text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-black border-b border-gray-700'>
+        <div className='text-[#d9d9d9] flex items-center sm:justify-between py-2 px-3 sticky top-0 z-50 bg-[#000000aa] border-b border-gray-700 backdrop-blur'>
           <h2 className='text-lg sm:text-xl font-bold'>Home</h2>
           <div className='hoverAnimation w-9 h-9 flex items-center justify-center xl:px-0 ml-auto'>
             <HiOutlineSparkles className='h-5 text-white' />
@@ -31,7 +32,15 @@ const Feed = () => {
         </div>
 
         <Input />
-        <div className='pb-72' />
+        <div className='pb-72'>
+          {posts.map(post => (
+            <Posts
+              id={post.id}
+              key={post.id}
+              post={post.data()}
+            />
+          ))}
+        </div>
       </div>
     </>
   )
